58bbefed6c6ab8ac8431f10aec57a476e8d44222
Josep Mª Pallàs Batalla <89463715+BasementHalfStackDev@users.noreply.github.com>
2023-03-24 16:22:29 +0100

    Add files via upload

---
+++ SQL Queries/Ejercicio_6_Biblioteca_V2_CreateDB.sql
@@ -0,0 +1,58 @@
+create database biblioteca_v2;
+
+use biblioteca_v2;
+
+CREATE TABLE socio(
+	DNI VARCHAR(10) PRIMARY KEY,
+	nombre VARCHAR(20) NOT NULL,
+	apellidos VARCHAR(20) NOT NULL,
+	codigo_socio INTEGER UNIQUE NOT NULL,
+	direccion VARCHAR(50) NOT NULL,
+	telefono VARCHAR(15) NOT NULL
+);
+
+CREATE TABLE libro(
+	codigo INTEGER PRIMARY KEY AUTO_INCREMENT,
+	nombre VARCHAR(60) NOT NULL,
+	año SMALLINT NOT NULL,
+	resumen VARCHAR(300) NOT NULL,
+	nombre_autor VARCHAR(50) NOT NULL,
+	pais_autor VARCHAR(30) NOT NULL,
+	deterioro VARCHAR(30),
+	num_paginas SMALLINT NOT NULL
+);
+
+CREATE TABLE CD(
+	codigo INTEGER PRIMARY KEY AUTO_INCREMENT,
+	nombre VARCHAR(60) NOT NULL,
+	año SMALLINT NOT NULL,
+	resumen VARCHAR(300) NOT NULL,
+	nombre_autor VARCHAR(50)NOT NULL,
+	pais_autor VARCHAR(30) NOT NULL,
+	deterioro VARCHAR(30),
+	num_canciones SMALLINT NOT NULL
+);
+
+CREATE TABLE pelicula(
+	codigo INTEGER PRIMARY KEY AUTO_INCREMENT,
+	nombre VARCHAR(60) NOT NULL,
+	año SMALLINT NOT NULL,
+	resumen VARCHAR(300) NOT NULL,
+	nombre_autor VARCHAR(50) NOT NULL,
+	pais_autor VARCHAR(30) NOT NULL,
+	deterioro VARCHAR(30),
+	duracion SMALLINT NOT NULL /* en minutos */
+);
+
+CREATE TABLE prestamo(
+	fecha DATE NOT NULL /* YYYY-MM-DD Format */,
+	articulo INT NOT NULL,
+	socio VARCHAR(10) NOT NULL,
+	fecha_devolucion DATE NOT NULL /* YYYY-MM-DD Format */, 
+	PRIMARY KEY (fecha, articulo, socio),
+	FOREIGN KEY (articulo) REFERENCES pelicula(codigo),
+	FOREIGN KEY (articulo) REFERENCES CD(codigo),
+	FOREIGN KEY (articulo) REFERENCES libro(codigo),
+	FOREIGN KEY (socio) REFERENCES socio(DNI)
+	ON UPDATE CASCADE
+);
